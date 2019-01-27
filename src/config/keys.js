5065633d447c466c66f12dff2f26c64b8436e3f8
Rasmus Laine <rasmus.laine@gmail.com>
2019-01-27 16:45:24 +0200

    added try catch block

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,7 +1,9 @@
-console.log(process.env.NODE_ENV);
-
 if (window.location.href.includes("localhost")) {
-  module.exports = require("./dev");
+  try {
+    module.exports = require("./dev");
+  } catch (e) {
+    console.error(e);
+  }
 } else {
   module.exports = require("./prod");
 }
